ee7e7f5585c924fe5d756f04bed807558c13be31
diegonava <diego.nava77@hotmail.com>
2019-08-30 13:41:15 +0200

    Get to the page 1.0

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -3,7 +3,6 @@ resource "aws_acm_certificate" "certificate" {
   domain_name       = "*.${var.root_domain_name}"
   validation_method = "DNS"
 
-  // We also want the cert to be valid for the root domain even though we'll be
-  // redirecting to the www. domain immediately.
+  // We also want the cert to be valid for the root domain
   subject_alternative_names = [var.root_domain_name]
 }
